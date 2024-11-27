55d3817ddbfa13a21b786589d0905b69772bf934
Antonio Natusch <anthony2004951@gmail.com>
2024-11-27 16:29:45 -0400

    fix(ejercicio4): fixed wrong function execution order

--- ejercicio4/src/menu.rs
+++ ejercicio4/src/menu.rs
@@ -1,10 +1,10 @@
-use crate::read_write_lock_impl::run_readers_writers;
 use crate::barber_impl::run_barber_shop;
+use crate::read_write_lock_impl::run_readers_writers;
 
 pub async fn problem_12() {
-    run_readers_writers().await;
+    run_barber_shop().await;
 }
 
 pub async fn problem_13() {
-    run_barber_shop().await;
+    run_readers_writers().await;
 }
